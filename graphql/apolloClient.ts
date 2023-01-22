069f57934b6640e7e42b59d3d3a604b5bb7726d1
spope <spope851@gmail.com>
2023-01-22 17:45:44 +0800

    use vercel url in prod

--- graphql/apolloClient.ts
+++ graphql/apolloClient.ts
@@ -1,21 +1,22 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { __prod__ } from "@/utils/env"
+import { ApolloClient, InMemoryCache } from "@apollo/client"
 
-const cache = new InMemoryCache();
+const cache = new InMemoryCache()
 
 const client = new ApolloClient({
   // Provide required constructor fields
   cache: cache,
-  uri: 'http://localhost:3000/api/graphql',
+  uri: `${__prod__ ? process.env.VERCEL_URL : "http://localhost:3000"}/api/graphql`,
 
   // Provide some optional constructor fields
-  name: 'react-web-client',
-  version: '1.3',
+  name: "react-web-client",
+  version: "1.3",
   queryDeduplication: false,
   defaultOptions: {
     watchQuery: {
-      fetchPolicy: 'cache-and-network',
+      fetchPolicy: "cache-and-network",
     },
   },
-});
+})
 
 export default client
