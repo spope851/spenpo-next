b06466027807ab348cc2bc8112f35405fdf5b755
spope <spope851@gmail.com>
2023-03-23 14:41:11 -0600

    rollback

--- components/projects/demos/reactTimeclockDemo.tsx
+++ components/projects/demos/reactTimeclockDemo.tsx
@@ -1,10 +1,16 @@
+import { useState, ReactNode, useEffect } from "react"
 import { Timeclock } from "react-timeclock"
 
 export default function ReactTimeclockDemo() {
+  const [timeclock, setTimeclock] = useState<ReactNode>()
+
+  useEffect(() => {
+    setTimeclock(<Timeclock />)
+  }, [])
   return (
     <td className="projects-table-data">
       <div id="react-timeclock-demo" className="app-demo">
-        <Timeclock />
+        {timeclock}
       </div>
     </td>
   )
