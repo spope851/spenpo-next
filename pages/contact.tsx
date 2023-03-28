3ae54f98c690ac4f7de6c5b9ff45b41e42f356ad
spope <spope851@gmail.com>
2023-03-27 20:57:48 -0600

    fix spacing bug on contact page

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -20,7 +20,7 @@ export default function Home() {
         <Header variant="h6">did you read my blog?</Header>
         <Header variant="h6">like one of my projects?</Header>
         <Header variant="h6">interested in working together?</Header>
-        <Box mb={{ xs: 5 }}>
+        <Box my={{ xs: 5 }}>
           <ContactForm />
         </Box>
       </Box>
