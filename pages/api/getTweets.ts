88d094390633d75998f077474da49b22bd8fe1c0
spope <spope851@gmail.com>
2023-03-29 15:41:24 -0600

    cache tweets and mandarin words in redis

--- pages/api/getTweets.ts
+++ pages/api/getTweets.ts
@@ -1,3 +1,4 @@
+import redis from "@/utils/redis"
 import type { NextApiRequest, NextApiResponse } from "next"
 
 const getTweets = async (
@@ -5,8 +6,11 @@ const getTweets = async (
   res: NextApiResponse<Record<string, any>>
 ) => {
   console.log("GET tweets  ", req.query)
+  const count = req.query.count
+  const cachedTweets = await redis.get(`tweets/${count}`)
+  if (cachedTweets) return res.send(JSON.parse(cachedTweets))
   return await fetch(
-    `https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=s_pop3&count=${req.query.count}&tweet_mode=extended`,
+    `https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=s_pop3&count=${count}&tweet_mode=extended`,
     {
       headers: {
         Authorization: `Bearer ${process.env.TWITTER_BEARER}`,
@@ -14,7 +18,10 @@ const getTweets = async (
     }
   )
     .then((data) => data.json())
-    .then((r) => res.send(r))
+    .then(async (tweets) => {
+      await redis.setex(`tweets/${count}`, 60, JSON.stringify(tweets))
+      return res.send(tweets)
+    })
 }
 
 export default getTweets
