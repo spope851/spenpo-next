9f07f1b4f5c2e1286b788d0ef8d0cfc3b49696dd
spope <spope@blockchains.com>
2023-10-22 17:29:39 -0600

    change aws access key var name

--- pages/api/get-signed-s3-url.ts
+++ pages/api/get-signed-s3-url.ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next"
 import AWS from "aws-sdk"
 
 const s3 = new AWS.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY_SPENPO,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   signatureVersion: "v4",
 })
