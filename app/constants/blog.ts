687744feb6a2d68c304f1dcba0f2ca4d566a533d
Spencer Pope <spenpo@spenpo.com>
2024-01-26 11:34:56 -0700

    og image for blogpost 159

--- app/constants/blog.ts
+++ app/constants/blog.ts
@@ -5,6 +5,8 @@ const previewImages: { [key: string]: string } & { default: string } = {
     'https://images.unsplash.com/photo-1525545575852-94dac00e6836?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=2000&fit=max&ixid=eyJhcHBfaWQiOjExNzczfQ',
   '156':
     'https://media.licdn.com/dms/image/C4E12AQGHInBIy9bh2g/article-cover_image-shrink_600_2000/0/1544044221946?e=2147483647&v=beta&t=W9n_d_DOSj5LtKLUquoC9_VEB_4lMZhFW-FHvH_ONiw',
+  '159':
+    'https://www.wiseradvisor.com/blog/wp-content/uploads/2019/08/Building-Wealth-660x400.jpg',
 }
 
 export { previewImages }
