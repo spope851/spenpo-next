8472aee33a92a91a140b7604ade33c5599b94778
Spencer Pope <spenpo@spenpo.com>
2024-02-06 09:45:39 -0700

    add privacy policy

---
+++ app/privacy/page.tsx
@@ -0,0 +1,224 @@
+import React from 'react'
+import { Box, Stack, Typography } from '@mui/material'
+
+export default async function Privacy() {
+  return (
+    <Stack p={{ sm: 5, xs: 2 }} gap={5} mx="auto" maxWidth="50em">
+      <Typography component="h1" display="flex" alignItems="baseline" gap={5}>
+        Privacy Policy
+      </Typography>
+      <Typography variant="body2">
+        This Privacy Policy (&quot;Policy&quot;) applies to spenpo.com, and Spencer
+        Pope (&quot;Company&quot;) and governs data collection and usage. For the
+        purposes of this Privacy Policy, unless otherwise noted, all references to
+        the Company include https://www.spenpo.com. The Company&apos;s application is
+        a web design application. By using the Company application, you consent to
+        the data practices described in this statement.
+      </Typography>
+      <Typography component="h1">Collection of your Personal Information</Typography>
+      <Typography variant="body2">
+        In order to better provide you with products and services offered, the
+        Company may collect personally identifiable information, such as your:
+      </Typography>
+      <Typography variant="body2" component="ul">
+        <Box component="li">First and last name</Box>
+        <Box component="li">Mailing address</Box>
+        <Box component="li">Email address</Box>
+        <Box component="li">Phone number</Box>
+      </Typography>
+      <Typography variant="body2">
+        If you purchase the Company&apos;s products and services, we collect billing
+        and credit card information. This information is used to complete the
+        purchase transaction.
+      </Typography>
+      <Typography variant="body2">
+        We do not collect any personal information about you unless you voluntarily
+        provide it to us. However, you may be required to provide certain personal
+        information to us when you elect to use certain products or services. These
+        may include: (a) registering for an account; (b) entering a sweepstakes or
+        contest sponsored by us or one of our partners; (c) signing up for special
+        offers from selected third parties; (d) sending us an email message; (e)
+        submitting your credit card or other payment information when ordering and
+        purchasing products and services. To wit, we will use your information for,
+        but not limited to, communicating with you in relation to services and/or
+        products you have requested from us. We also may gather additional personal
+        or non-personal information in the future.
+      </Typography>
+      <Typography component="h1">Use of your Personal Information</Typography>
+      <Typography variant="body2">
+        The Company collects and uses your personal information in the following
+        ways:
+      </Typography>
+      <Typography variant="body2" component="ul">
+        <Box component="li">
+          to operate and deliver the services you have requested
+        </Box>
+        <Box component="li">
+          to provide you with information, products, or services that you request
+          from us
+        </Box>
+        <Box component="li">to provide you with notices about your account</Box>
+        <Box component="li">
+          to carry out the Company&apos;s obligations and enforce our rights arising
+          from any contracts entered between you and us, including for billing and
+          collection
+        </Box>
+        <Box component="li">
+          to notify you about changes to our spenpo.com or any products or services
+          we offer or provide through it
+        </Box>
+        <Box component="li">
+          in any other way we may describe when you provide the information
+        </Box>
+        <Box component="li">for any other purpose with your consent.</Box>
+      </Typography>
+      <Typography variant="body2">
+        The Company may also use your personally identifiable information to inform
+        you of other products or services available from the Company and its
+        affiliates.
+      </Typography>
+      <Typography component="h1" display="flex" alignItems="baseline" gap={5}>
+        Sharing Information with Third Parties
+      </Typography>
+      <Typography variant="body2">
+        The Company does not sell, rent, or lease its customer lists to third
+        parties.
+      </Typography>
+      <Typography variant="body2">
+        The Company may share data with trusted partners to help perform statistical
+        analysis, send you email or postal mail, provide customer support, or arrange
+        for deliveries. All such third parties are prohibited from using your
+        personal information except to provide these services tothe Company, and they
+        are required to maintain the confidentiality of your information.
+      </Typography>
+      <Typography variant="body2">
+        The Company may disclose your personal information, without notice, if
+        required to do so by law or in the good faith belief that such action is
+        necessary to: (a) conform to the edicts of the law or comply with legal
+        process served on the Company or the site; (b) protect and defend the rights
+        or property of the Company; and/or (c) act under exigent circumstances to
+        protect the personal safety of users of the Company, or the public.
+      </Typography>
+      <Typography component="h1" display="flex" alignItems="baseline" gap={5}>
+        Right to Deletion
+      </Typography>
+      <Typography variant="body2">
+        Subject to certain exceptions set out below, on receipt of a verifiable
+        request from you, we will:
+      </Typography>
+      <Typography variant="body2" component="ul">
+        <Box component="li">
+          Delete your personal information from our records; and
+        </Box>
+        <Box component="li">
+          Direct any service providers to delete your personal information from their
+          records.
+        </Box>
+      </Typography>
+      <Typography variant="body2">
+        Please note that we may not be able to comply with requests to delete your
+        personal information if it is necessary to:
+      </Typography>
+      <Typography variant="body2" component="ul">
+        <Box component="li">
+          Complete the transaction for which the personal information was collected,
+          fulfill the terms of a written warranty or product recall conducted in
+          accordance with federal law, and provide a good or service requested by
+          you, or reasonably anticipated within the context of our ongoing business
+          relationship with you, or otherwise perform a contract between you and us;
+        </Box>
+        <Box component="li">
+          Detect security incidents, protect against malicious, deceptive,
+          fraudulent, or illegal activity; or prosecute those responsible for that
+          activity;
+        </Box>
+        <Box component="li">
+          Debug to identify and repair errors that impair existing intended
+          functionality;
+        </Box>
+        <Box component="li">
+          Exercise free speech, ensure the right of another consumer to exercise his
+          or her right of free speech, or exercise another right provided for by law;
+        </Box>
+        <Box component="li">
+          Comply with the California Electronic Communications Privacy Act;
+        </Box>
+        <Box component="li">
+          Engage in public or peer-reviewed scientific, historical, or statistical
+          research in the public interest that adheres to all other applicable ethics
+          and privacy laws, when our deletion of the information is likely to render
+          impossible or seriously impair the achievement of such research, provided
+          we have obtained your informed consent;
+        </Box>
+        <Box component="li">
+          Enable solely internal uses that are reasonably aligned with your
+          expectations based on your relationship with us;
+        </Box>
+        <Box component="li">Comply with an existing legal obligation; or</Box>
+        <Box component="li">
+          Otherwise use your personal information, internally, in a lawful manner
+          that is compatible with the context in which you provided the information.
+        </Box>
+      </Typography>
+      <Typography component="h1" display="flex" alignItems="baseline" gap={5}>
+        Children Under Thirteen
+      </Typography>
+      <Typography variant="body2">
+        The Company does not knowingly collect personally identifiable information
+        from children under the age of 13. If you are under the age of 13, you must
+        ask your parent or guardian for permission to use this application.
+      </Typography>
+      <Typography component="h1" display="flex" alignItems="baseline" gap={5}>
+        Email Communications
+      </Typography>
+      <Typography variant="body2">
+        From time to time, the Company may contact you via email for the purpose of
+        providing announcements, promotional offers, alerts, confirmations, surveys,
+        and/or other general communication.
+      </Typography>
+      <Typography component="h1" display="flex" alignItems="baseline" gap={5}>
+        External Data Storage Sites
+      </Typography>
+      <Typography variant="body2">
+        We may store your data on servers provided by third-party hosting vendors
+        with whom we have contracted.
+      </Typography>
+      <Typography component="h1" display="flex" alignItems="baseline" gap={5}>
+        Changes to This Statement
+      </Typography>
+      <Typography variant="body2">
+        The Company reserves the right to change this Policy from time to time. For
+        example, when there are changes in our services, changes in our data
+        protection practices, or changes in the law. When changes to this Policy are
+        significant, we will inform you. You may receive a notice by sending an email
+        to the primary email address specified in your account, by placing a
+        prominent notice on our Spencer Pope, and/or by updating any privacy
+        information. Your continued use of the application and/or services available
+        after such modifications will constitute your: (a) acknowledgment of the
+        modified Policy; and (b) agreement to abide and be bound by that Policy.
+      </Typography>
+      <Typography component="h1" display="flex" alignItems="baseline" gap={5}>
+        Contact Information
+      </Typography>
+      <Typography variant="body2">
+        The Company welcomes your questions or comments regarding this Policy. If you
+        believe that the Company has not adhered to this Policy, please contact the
+        Company at:
+      </Typography>
+      <Box>
+        <Typography variant="body2">Spencer Pope</Typography>
+        <Typography variant="body2">132 S College Ave</Typography>
+        <Typography variant="body2">FORT COLLINS, Colorado 80524</Typography>
+      </Box>
+      <Box>
+        <Typography variant="body2">Email Address:</Typography>
+        <Typography variant="body2">spenpo@spenpo.com</Typography>
+      </Box>
+      <Box>
+        <Typography variant="body2">Phone Number:</Typography>
+        <Typography variant="body2">9546940844</Typography>
+      </Box>
+      <Typography variant="body2">Effective as of February 06, 2024</Typography>
+    </Stack>
+  )
+}
