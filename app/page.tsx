824c7758a7bf4f5dac5a6ae139c34830a21788b8
Spencer Pope <spenpo@spenpo.com>
2024-03-01 09:35:39 -0700

    update linkedin profile link

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Root() {
         'mailto:spenpo@spenpo.com',
         'https://www.youtube.com/@spope',
         'https://www.twitch.tv/spenpo',
-        'https://www.linkedin.com/in/spencer-pope-1b7a1411b/',
+        'https://www.linkedin.com/in/spenpo/',
       ]}
       headshotSrc="/images/headshot.jpeg"
       actionDestination="products/landing-page"
