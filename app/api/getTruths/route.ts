0fbe3f829b209ba46a6740ecf820e7fe67ab1717
Spencer Pope <spenpo@spenpo.com>
2025-02-21 19:08:59 +0800

    fix truths route with new table name

--- app/api/getTruths/route.ts
+++ app/api/getTruths/route.ts
@@ -6,9 +6,9 @@ export const dynamic = 'force-dynamic'
 
 export async function GET() {
   const truths: unknown[] =
-    await prisma.$queryRaw`SELECT id, sentence, CASE when is_true then '1' else '0' end as is_true FROM "Truth" WHERE is_true ORDER BY random() LIMIT 2;`
+    await prisma.$queryRaw`SELECT id, sentence, CASE when is_true then '1' else '0' end as is_true FROM wp_spenpo_truth WHERE is_true ORDER BY RAND() LIMIT 2;`
   const lie: unknown[] =
-    await prisma.$queryRaw`SELECT id, sentence, CASE when is_true then '1' else '0' end as is_true FROM "Truth" WHERE not is_true ORDER BY random() LIMIT 1;`
+    await prisma.$queryRaw`SELECT id, sentence, CASE when is_true then '1' else '0' end as is_true FROM wp_spenpo_truth WHERE not is_true ORDER BY RAND() LIMIT 1;`
 
   return NextResponse.json(shuffle([...truths, ...lie]))
 }
